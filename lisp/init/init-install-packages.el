fd059733762a73198ae6ee288f66509abc6195db
Zach Jibben <zjibben@asu.edu>
2016-06-09 09:17:35 -0600

    Only update repo archive when it is empty

--- lisp/init/init-install-packages.el
+++ lisp/init/init-install-packages.el
@@ -4,12 +4,9 @@
 ;; this script will install emacs packages I use when they are not already found
 ;; 
 ;; inspired by http://stackoverflow.com/q/10092322
-;; note that several of the checks in that source are unnecessary,
-;; since package-install will fetch the list of packages if necessary,
-;; and checks if a package is already installed before installing
+;; note package-install checks if a package is already installed before installing
 ;;
-;; zjibben <threeofsix@gmail.com> 12/2015
-;;
+;; zjibben <threeofsix@gmail.com>
 
 ;; list of packages to have installed
 (setq package-install-list '(
@@ -38,7 +35,8 @@
                      ;use-package
                      ))
 
-;; install all the above packages
+;; install all the above packages, updating the package archive if necessary
+(unless package-archive-contents (package-refresh-contents))
 (dolist (package package-install-list) (package-install package))
 
 ;; TODO: patch latex-pretty-symbols
