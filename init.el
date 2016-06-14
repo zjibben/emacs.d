a7bf6f2f0825d33c431df91f290cec24b82a2321
Zach Jibben <zjibben@asu.edu>
2016-06-13 18:49:44 -0600

    Declare proxy-enable

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/init/")
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
+(setq proxy-enable nil)
 (require 'init-private-info nil t) ;; attempt to grab private info
 (when proxy-enable (require 'init-proxy))
 (require 'init-packages)
