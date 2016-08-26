d2418c09cf1bc8e88254217feb578c4ac5a886c8
Zach Jibben <zjibben@lanl.gov>
2016-08-25 20:10:02 -0600

    Calculate font size from display size and resolution

--- lisp/init/init-theme.el
+++ lisp/init/init-theme.el
@@ -7,18 +7,26 @@
 
 ;;(setq color-theme-is-global nil) ;; only for color-theme package?
 
+;; select the font size based on screen size and resolution
+(defun font-size ()
+  (let ((mm-width (nth 1 (assoc 'mm-size (frame-monitor-attributes)))))
+  (if (>= mm-width 600)
+      11
+    (cond
+     ((<= (display-pixel-width) 1366)  9)
+     ((>= (display-pixel-width) 2560) 15)
+     (t 11)))))
+
 ;; define a function to set the theme based on the mode (gui/console)
 ;; and load it every time we open a new frame
 (defun set-theme (&optional frame)
   (if frame (select-frame frame))
   (load-theme 'ample t)
   (if (window-system frame)
-      (progn  ;; then
-        (if (<= (display-pixel-width) 1366)
-            (set-frame-font "Monospace 9")
-          (set-frame-font "Monospace 11"))
-        (powerline-default-theme) )
-    (progn  ;; else
+      (progn
+        (set-frame-font (concat "Monospace " (number-to-string (font-size))))
+        (powerline-default-theme))
+    (progn
       ;;(disable-theme 'ample) ;; in case it was active
       (set-face-background 'default "unspecified-bg" (selected-frame)) ;; terminal sets bg color
       (powerline-vim-theme) ) )
