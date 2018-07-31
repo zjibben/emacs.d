a271d3154bfe54474baf6e653e11d8c73b59d087
Zach Jibben <zjibben@lanl.gov>
2018-07-31 10:21:53 -0600

    Clean package installation

--- lisp/init/init-install-packages.el
+++ lisp/init/init-install-packages.el
@@ -1,78 +1,49 @@
-;;
 ;; install packages
 ;;
-;; this script will install emacs packages I use when they are not already found
-;; 
-;; inspired by http://stackoverflow.com/q/10092322
-;; note package-install checks if a package is already installed before installing
-;;
-;; zjibben <threeofsix@gmail.com>
+;; install emacs packages if they aren't already installed
 
 ;; list of packages to have installed
-(setq package-selected-packages '(
-                     adaptive-wrap
-                     ample-theme
-                     arduino-mode
-                     auctex
-                     cmake-mode
-                     djvu
-                     eimp
-                     elpy
-                     ;ein
-                     fill-column-indicator
-                     flx-ido
-                     gnuplot-mode
-                     haskell-mode
-                     ;helm
-                     julia-mode
-                     ;latex-pretty-symbols
-                     ;latex-preview-pane
-                     magit
-                     markdown-mode
-                     ;multi-term
-                     ;org-bullets
-                     pdf-tools
-                     pinentry
-                     pkgbuild-mode
-                     plantuml-mode
-                     powerline
-                     powershell
-                     python-info
-                     rust-mode
-                     smex
-                     use-package
-                     wordnut
-                     yaml-mode
-                     ))
+(setq package-selected-packages
+      '(
+        adaptive-wrap
+        ample-theme
+        arduino-mode
+        auctex
+        cmake-mode
+        djvu
+        eimp
+        elpy
+        ;;ein
+        fill-column-indicator
+        flx-ido
+        gnuplot-mode
+        haskell-mode
+        julia-mode
+        magit
+        markdown-mode
+        ;;multi-term
+        pdf-tools
+        pinentry
+        pkgbuild-mode
+        plantuml-mode
+        powerline
+        powershell
+        python-info
+        rust-mode
+        smex
+        use-package
+        wordnut
+        yaml-mode
+        ))
 
 ;; install all the above packages, updating the package archive if necessary
 (if (>= (string-to-number emacs-version) 25)
-    (package-refresh-contents)
-    (package-install-selected-packages)
-    (progn
-      (unless package-archive-contents (package-refresh-contents))
-      (dolist (package package-selected-packages) (package-install package))))
+    (progn (package-refresh-contents)
+	   (package-install-selected-packages))
 
-;; TODO: patch latex-pretty-symbols
-
-;; other packages worth noting:
-;;   achievements
-;;   color-theme
-;;   tuareg
-;;   multi-term
-;;   helm
-;;   ivy
-;;   latex-preview-pane
-;;   org-bullets
-
-;; other themes:
-;;   almost-monokai
-;;   base16-theme
-;;   boron-theme
-;;   darcula-theme
-;;   monokai-theme
-;;   solarized-theme
-;;   sublime-themes
-;;   zenburn-theme
+  (progn (package-refresh-contents)
+         (dolist (package package-selected-packages)
+           (unless (package-installed-p package)
+             (package-install package)))))
 
 (provide 'init-install-packages)
