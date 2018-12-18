7daf1cdcc399746ccf8e08834a9098bd49e4d0a9
Zach Jibben <zjibben@lanl.gov>
2018-12-18 11:10:46 -0700

    Add keyboard shortcut for reversing through kill ring

--- lisp/init/init-config.el
+++ lisp/init/init-config.el
@@ -126,6 +126,7 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "M-o") 'next-multiframe-window)
 (global-set-key (kbd "M-O") 'previous-multiframe-window)
+(global-set-key (kbd "M-Y") (lambda () (interactive) (yank-pop -1)))
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "C-M-s") 'isearch-forward)
